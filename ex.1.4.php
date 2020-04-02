f297889427154a503f287023e630f6205a117438
unknown <ngochua99h@gmail.com>
2020-04-02 22:53:17 +0700

    homework 1.4

---
+++ ex.1.4.php
@@ -0,0 +1,22 @@
+<?php 
+$arr = array(1,4,2,6,9,100,45,8,20); 
+
+ 
+$max = null;
+ 
+for ($i = 0; $i < count($arr); $i++)
+{
+    if ($max == null){
+        $max = $arr[$i];
+        
+    }
+    else {
+        if ($arr[$i] > $max){
+            $max = $arr[$i];
+            
+        }
+    }
+}
+ 
+echo "Giá trị lớn nhất là $max";
+ ?>
