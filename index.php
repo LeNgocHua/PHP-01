71d33cea538091d1578e4983f7854e101db5dd05
LeNgocHua <63055839+LeNgocHua@users.noreply.github.com>
2020-04-06 14:13:00 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ $dbname = "blogs";
 
 // Tạo kết nối CSDL
 $conn = new mysqli($severname, $username, $password, $dbname);
-
+$conn -> set_charset("utf8");
 // Câu lệnh truy vấn.
-$query = "SELECT * FROM post  LIMIT 6";
+$query = "SELECT * FROM post  LIMIT 6 ";
 
 //Thực thi câu lệnh
 $result = $conn->query($query);
@@ -21,12 +21,24 @@ $post = array();
 while ($row = $result->fetch_assoc()) {
 	$post[] = $row;
 }
- ?>
+// Câu lệnh truy vấn.
+$query = "SELECT * FROM post  LIMIT 6,7";
 
+//Thực thi câu lệnh
+$result = $conn->query($query);
+
+//Tạo mảng để chứa dữ liệu.
+$a_post = array();
+
+while ($row = $result->fetch_assoc()) {
+	$a_post[] = $row;
+}
+
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8">
+		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
@@ -228,112 +240,47 @@ while ($row = $result->fetch_assoc()) {
 					<div class="col-md-8">
 						<div class="row">
 							<!-- post -->
+							
+
+                            <?php foreach ($a_post as $posts) {
+	
+	            ?>
+							
 							<div class="col-md-12">
 								<div class="post post-thumb">
-									<a class="post-img" href="blog-post.html"><img src="./img/post-2.jpg" alt=""></a>
+									<a class="post-img" href="blog-post.html"><img src="<?php echo $posts['thumbnail']; ?>" alt=""></a>
 									<div class="post-body">
 										<div class="post-meta">
 											<a class="post-category cat-3" href="category.html">Jquery</a>
-											<span class="post-date">March 27, 2018</span>
+											<span class="post-date"><?php echo $posts['created_at']; ?></span>
 										</div>
-										<h3 class="post-title"><a href="blog-post.html">Ask HN: Does Anybody Still Use JQuery?</a></h3>
+										<h3 class="post-title"><a href="blog-post.html"><?php echo $posts['title']; ?></a></h3>
 									</div>
 								</div>
 							</div>
 							<!-- /post -->
-
+                             <?php } ?>
 							<!-- post -->
-							<div class="col-md-6">
-								<div class="post">
-									<a class="post-img" href="blog-post.html"><img src="./img/post-1.jpg" alt=""></a>
-									<div class="post-body">
-										<div class="post-meta">
-											<a class="post-category cat-4" href="category.html">Css</a>
-											<span class="post-date">March 27, 2018</span>
-										</div>
-										<h3 class="post-title"><a href="blog-post.html">CSS Float: A Tutorial</a></h3>
-									</div>
+							   <?php foreach ($post as $posts) {
+	
+	            ?>
+                      
+					<!-- post -->
+					<div class="col-md-4">
+						<div class="post">
+							<a class="post-img" href="blog-post.html"><img src="<?php echo $posts['thumbnail']; ?>" style =" height: 300px;" alt=""></a>
+							<div class="post-body">
+								<div class="post-meta">
+									<a class="post-category cat-1" href="category.html">Web Design</a>
+									<span class="post-date"><?php echo $posts['created_at']; ?></span>
 								</div>
+								<h3 class="post-title"><a href="blog-post.html"><?php echo $posts['title']; ?></a></h3>
 							</div>
-							<!-- /post -->
-
-							<!-- post -->
-							<div class="col-md-6">
-								<div class="post">
-									<a class="post-img" href="blog-post.html"><img src="./img/post-2.jpg" alt=""></a>
-									<div class="post-body">
-										<div class="post-meta">
-											<a class="post-category cat-1" href="category.html">Web Design</a>
-											<span class="post-date">March 27, 2018</span>
-										</div>
-										<h3 class="post-title"><a href="blog-post.html">Tell-A-Tool: Guide To Web Design And Development Tools</a></h3>
-									</div>
-								</div>
-							</div>
-							<!-- /post -->
-
-							<div class="clearfix visible-md visible-lg"></div>
-
-							<!-- post -->
-							<div class="col-md-6">
-								<div class="post">
-									<a class="post-img" href="blog-post.html"><img src="./img/post-4.jpg" alt=""></a>
-									<div class="post-body">
-										<div class="post-meta">
-											<a class="post-category cat-2" href="category.html">JavaScript</a>
-											<span class="post-date">March 27, 2018</span>
-										</div>
-										<h3 class="post-title"><a href="blog-post.html">Chrome Extension Protects Against JavaScript-Based CPU Side-Channel Attacks</a></h3>
-									</div>
-								</div>
-							</div>
-							<!-- /post -->
-
-							<!-- post -->
-							<div class="col-md-6">
-								<div class="post">
-									<a class="post-img" href="blog-post.html"><img src="./img/post-5.jpg" alt=""></a>
-									<div class="post-body">
-										<div class="post-meta">
-											<a class="post-category cat-3" href="category.html">Jquery</a>
-											<span class="post-date">March 27, 2018</span>
-										</div>
-										<h3 class="post-title"><a href="blog-post.html">Ask HN: Does Anybody Still Use JQuery?</a></h3>
-									</div>
-								</div>
-							</div>
-							<!-- /post -->
-
-							<div class="clearfix visible-md visible-lg"></div>
-
-							<!-- post -->
-							<div class="col-md-6">
-								<div class="post">
-									<a class="post-img" href="blog-post.html"><img src="./img/post-3.jpg" alt=""></a>
-									<div class="post-body">
-										<div class="post-meta">
-											<a class="post-category cat-1" href="category.html">Web Design</a>
-											<span class="post-date">March 27, 2018</span>
-										</div>
-										<h3 class="post-title"><a href="blog-post.html">Pagedraw UI Builder Turns Your Website Design Mockup Into Code Automatically</a></h3>
-									</div>
-								</div>
-							</div>
-							<!-- /post -->
-
-							<!-- post -->
-							<div class="col-md-6">
-								<div class="post">
-									<a class="post-img" href="blog-post.html"><img src="./img/post-4.jpg" alt=""></a>
-									<div class="post-body">
-										<div class="post-meta">
-											<a class="post-category cat-2" href="category.html">JavaScript</a>
-											<span class="post-date">March 27, 2018</span>
-										</div>
-										<h3 class="post-title"><a href="blog-post.html">Chrome Extension Protects Against JavaScript-Based CPU Side-Channel Attacks</a></h3>
-									</div>
-								</div>
-							</div>
+						</div>
+					</div>
+					<!-- /post -->
+				<?php } ?>
+							
 							<!-- /post -->
 						</div>
 					</div>
